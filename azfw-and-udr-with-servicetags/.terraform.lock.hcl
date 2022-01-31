6ccdb38dc69537c74fbe96d5dfa258b82c3f0b88
Tsubasa Nomura <tsubasa@nomupro.com>
2022-02-01 01:06:50 +0900

    Add verification case

---
+++ azfw-and-udr-with-servicetags/.terraform.lock.hcl
@@ -0,0 +1,38 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version = "2.94.0"
+  hashes = [
+    "h1:iuMLg+/otmiIJ1Uas5W2aOd7XWwjn3K+yD6d6cOliC4=",
+    "zh:02d710ecca217193a18e50e9c203575584a316f1ddab84315ed39b3c6095626c",
+    "zh:02fffe19fd902f9f655ac351a3e9ab568554410d0b3e60df9bd1a5307bc4616d",
+    "zh:0cb8e9c834790834f16e74539245f022bc485d3c683099aa169e55b8cc3bbb69",
+    "zh:13484f97f7a8d194dd57e4332817bde5c7e1bb7b000846ba7232c7da9478f3a8",
+    "zh:17bf1c0716c7df1919c884e84b66e59d30d95ab169d72c9bbb33ed196dc16f22",
+    "zh:1f152c660c33217c55f933eafac0ca2c08a532e9ec2f9d64f3039f638334bca1",
+    "zh:ba344559c2db93dd21e2038b21d92978558247a44ba01b1e1672279083a7708b",
+    "zh:bf08d097cbea5eed1bd2bdc5e447f162393f12eed5d9b9aaf2dc6c9d402858c6",
+    "zh:c5268e5093a2fa17f68c97f7a9934c95645e71538e10d2edd919b91c96060bfd",
+    "zh:d5cffe43a8015ca2ab910eac307203d5bc361f565d2ac4d38231a854555b1403",
+    "zh:e71fb62706200765e3d571f4c34d17f0ab803e01fc592a48445b7b1a7b4cd98c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.1.0"
+  hashes = [
+    "h1:BZMEPucF+pbu9gsPk0G0BHx7YP04+tKdq2MrRDF1EDM=",
+    "zh:2bbb3339f0643b5daa07480ef4397bd23a79963cc364cdfbb4e86354cb7725bc",
+    "zh:3cd456047805bf639fbf2c761b1848880ea703a054f76db51852008b11008626",
+    "zh:4f251b0eda5bb5e3dc26ea4400dba200018213654b69b4a5f96abee815b4f5ff",
+    "zh:7011332745ea061e517fe1319bd6c75054a314155cb2c1199a5b01fe1889a7e2",
+    "zh:738ed82858317ccc246691c8b85995bc125ac3b4143043219bd0437adc56c992",
+    "zh:7dbe52fac7bb21227acd7529b487511c91f4107db9cc4414f50d04ffc3cab427",
+    "zh:a3a9251fb15f93e4cfc1789800fc2d7414bbc18944ad4c5c98f466e6477c42bc",
+    "zh:a543ec1a3a8c20635cf374110bd2f87c07374cf2c50617eee2c669b3ceeeaa9f",
+    "zh:d9ab41d556a48bd7059f0810cf020500635bfc696c9fc3adab5ea8915c1d886b",
+    "zh:d9e13427a7d011dbd654e591b0337e6074eef8c3b9bb11b2e39eaaf257044fd7",
+    "zh:f7605bd1437752114baf601bdf6931debe6dc6bfe3006eb7e9bb9080931dca8a",
+  ]
+}
---
+++ azfw-and-udr-with-servicetags/main.tf
@@ -0,0 +1,373 @@
+variable "resource_group_name" {
+  default = "rg-azfw"
+}
+variable "location" {
+  default = "japaneast"
+}
+variable "application_rule_collection_rule1" {
+  default = [
+    "*.teams.microsoft.com",
+    "login.microsoftonline.com",
+    "aadcdn.msftauth.net",
+    "autologon.microsoftazuread-sso.com",
+    "aadcdn.msauthimages.net",
+    "statics.teams.cdn.office.net",
+    "*.skype.com",
+    "teams.microsoft.com",
+    "outlook.office.com",
+    "graph.microsoft.com",
+    "substrate.office.com",
+    "spoprod-a.akamaihd.net",
+    "ifconfig.me",
+  ]
+}
+variable "admin_username" {
+  default = "azureuser"
+}
+variable "admin_password" {
+  default = "Passw0rd1!"
+}
+variable "myipaddress" {
+
+}
+
+
+terraform {
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "random_string" "uniqstr" {
+  length  = 6
+  special = false
+  upper   = false
+  keepers = {
+    resource_group_name = azurerm_resource_group.example.name
+  }
+}
+
+#################
+# Log Analytics
+#################
+resource "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "la-aksdemo${random_string.uniqstr.result}"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+}
+
+#################
+# VNET
+#################
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet-example"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "azfw" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+#################
+# Route Table
+#################
+
+resource "azurerm_route_table" "example" {
+  name                = "route-azfw"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_route" "azfw" {
+  name                   = "toAppService"
+  resource_group_name    = azurerm_resource_group.example.name
+  route_table_name       = azurerm_route_table.example.name
+  address_prefix         = "AppService"
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = azurerm_firewall.example.ip_configuration.0.private_ip_address
+}
+resource "azurerm_route" "internet" {
+  name                = "toInternet"
+  resource_group_name = azurerm_resource_group.example.name
+  route_table_name    = azurerm_route_table.example.name
+  address_prefix      = "0.0.0.0/0"
+  next_hop_type       = "Internet"
+}
+resource "azurerm_route" "avd" {
+  name                   = "toAVD"
+  resource_group_name    = azurerm_resource_group.example.name
+  route_table_name       = azurerm_route_table.example.name
+  address_prefix         = "WindowsVirtualDesktop"
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = azurerm_firewall.example.ip_configuration.0.private_ip_address
+}
+
+resource "azurerm_subnet_route_table_association" "example" {
+  subnet_id      = azurerm_subnet.default.id
+  route_table_id = azurerm_route_table.example.id
+}
+
+#################
+# Windows VM
+#################
+resource "azurerm_public_ip" "winvm" {
+  name                = "pip-winvm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Standard"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "winvm" {
+  name                = "nic-winvm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.winvm.id
+  }
+}
+resource "azurerm_windows_virtual_machine" "windows" {
+  name                = "winvm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B2ms"
+  zone                = 1
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.winvm.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-winvm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  security_rule {
+    name                       = "remote_access"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.winvm.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_dev_test_global_vm_shutdown_schedule" "autoshutdown" {
+  virtual_machine_id = azurerm_windows_virtual_machine.windows.id
+  location           = azurerm_resource_group.example.location
+  enabled            = true
+
+  daily_recurrence_time = "0300"
+  timezone              = "Tokyo Standard Time"
+
+  notification_settings {
+    enabled = false
+  }
+}
+
+#######################
+# Azure Firewall
+#######################
+
+resource "azurerm_public_ip" "example" {
+  name                = "pip-azfw"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "example" {
+  name                = "azfw"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.azfw.id
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_firewall_application_rule_collection" "example" {
+  name                = "rule1"
+  azure_firewall_name = azurerm_firewall.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  priority            = 500
+  action              = "Allow"
+
+  dynamic "rule" {
+    for_each = concat(var.application_rule_collection_rule1, [azurerm_app_service.example.default_site_hostname])
+    content {
+      name             = rule.value
+      source_addresses = ["*"]
+      target_fqdns     = [rule.value]
+      protocol {
+        port = "443"
+        type = "Https"
+      }
+
+    }
+  }
+}
+
+resource "azurerm_firewall_nat_rule_collection" "example" {
+  name                = "rule1"
+  azure_firewall_name = azurerm_firewall.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  priority            = 500
+  action              = "Dnat"
+
+  rule {
+    name = "RDP"
+
+    source_addresses = [
+      var.myipaddress
+    ]
+
+    destination_ports = [
+      "3389",
+    ]
+
+    destination_addresses = [
+      azurerm_public_ip.example.ip_address
+    ]
+
+    translated_port = 3389
+
+    translated_address = azurerm_network_interface.winvm.private_ip_address
+
+    protocols = [
+      "TCP",
+    ]
+  }
+}
+
+data "azurerm_monitor_diagnostic_categories" "azfw" {
+  resource_id = azurerm_firewall.example.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "azfw" {
+  name                       = "diag"
+  target_resource_id         = azurerm_firewall.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+
+  dynamic "log" {
+    for_each = data.azurerm_monitor_diagnostic_categories.azfw.logs
+    content {
+      category = log.value
+      enabled  = true
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+}
+
+
+#######################
+# App Service
+#######################
+resource "azurerm_app_service_plan" "example" {
+  name                = "appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "apps${random_string.uniqstr.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "NODE|16-lts"
+  }
+}
+
+data "azurerm_monitor_diagnostic_categories" "webapps" {
+  resource_id = azurerm_app_service.example.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "webapps" {
+  name                       = "diag"
+  target_resource_id         = azurerm_app_service.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+
+  dynamic "log" {
+    for_each = data.azurerm_monitor_diagnostic_categories.webapps.logs
+    content {
+      category = log.value
+      enabled  = true
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+}
+
