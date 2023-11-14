7d69f6390a812b875a1b282dbe2a67c0c0aeae25
Tsubasa Nomura <tsubasa@nomupro.com>
2023-11-14 08:22:34 +0000

    update

--- modules/diagnostic_logs/main.tf
+++ modules/diagnostic_logs/main.tf
@@ -6,26 +6,28 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic_logs" {
   target_resource_id = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id 
 
-  dynamic "log" {
+  # dynamic "log" {
+  #   for_each = var.diagnostic_logs
+  #   content {
+  #     category = log.value
+  #     enabled  = true
+
+  #     retention_policy {
+  #       enabled = true
+  #       days    = var.retention
+  #     }
+  #   }
+  # }
+
+  dynamic enabled_log {
     for_each = var.diagnostic_logs
     content {
-      category = log.value
-      enabled  = true
-
-      retention_policy {
-        enabled = true
-        days    = var.retention
-      }
+      category = enabled_log.value
     }
   }
 
   metric {
     category = "AllMetrics"
     enabled  = false
-
-    retention_policy {
-      days    = 0
-      enabled = false
-    }
   }
 }
